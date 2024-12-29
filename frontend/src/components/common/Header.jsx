75bf79ad33f07fde9610ddc6e27dac107905f72b
Hector <hectorc2907@gmail.com>
2024-12-29 20:30:44 -0300

    trabajamos en el menu desplegable para los dispositivos mobiles, tambien su logica y disposicion de apertura y cierra

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -7,25 +7,57 @@ const Header = () => {
 
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
-    console.log(menuOpen)
   };
   return (
-    <div className="flex justify-center">
-      <div className="container bg-red-200">
-        <div className="p-2 flex justify-between items-center">
-          <a href="/">
-            <img src={logoLight} alt="logoLight" className="w-[50px]" />
-          </a>
-          <div className="text-[50px]">
-            {menuOpen ? (
-              <IoClose onClick={() => toggleMenu()} />
-            ) : (
-              <IoMenu onClick={() => toggleMenu()} />
-            )}
+    <>
+      {menuOpen && (
+        <div
+          className="fixed z-10 inset-0 backdrop-blur-sm bg-black/30"
+          onClick={() => setMenuOpen(false)}
+        ></div>
+      )}
+
+      <div className="sticky z-30 top-0 flex justify-center">
+        <div className="container relative bg-slate-50">
+          <div className="p-2 flex justify-between items-center">
+            <a href="/">
+              <img src={logoLight} alt="logoLight" className="w-[50px]" />
+            </a>
+            <div className="text-[50px] cursor-pointer">
+              {menuOpen ? (
+                <IoClose onClick={() => toggleMenu()} />
+              ) : (
+                <IoMenu onClick={() => toggleMenu()} />
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      {menuOpen && (
+        <div className="absolute top-0 right-0 max-w-[425px] w-full h-screen pt-[67.92px] z-20 bg-slate-50">
+          <a href="#home">
+            <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
+              Home
+            </div>
+          </a>
+          <a href="#aboutMe">
+            <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
+              Sobre Mí
+            </div>
+          </a>
+          <a href="#project">
+            <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
+              Proyectos
+            </div>
+          </a>
+          <a href="#contact">
+            <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
+              Contactos
+            </div>
+          </a>
+        </div>
+      )}
+    </>
   );
 };
 
