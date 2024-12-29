b0afe9a1ca2df8774cf9905dd340ea7199194cdc
Hector <hectorc2907@gmail.com>
2024-12-29 19:38:46 -0300

    instalamos react-icons y configuramos un pequeño Header incluyendo los iconos

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
+import { IoClose, IoMenu } from "react-icons/io5";
+import logoLight from "../../assets/icons/logoLight.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen((prev) => !prev);
+    console.log(menuOpen)
+  };
+  return (
+    <div className="flex justify-center">
+      <div className="container bg-red-200">
+        <div className="p-2 flex justify-between items-center">
+          <a href="/">
+            <img src={logoLight} alt="logoLight" className="w-[50px]" />
+          </a>
+          <div className="text-[50px]">
+            {menuOpen ? (
+              <IoClose onClick={() => toggleMenu()} />
+            ) : (
+              <IoMenu onClick={() => toggleMenu()} />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
