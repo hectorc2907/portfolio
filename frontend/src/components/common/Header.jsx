d2b825a29301b3fb106b21cafdd8dd1f6b348405
Hector <hectorc2907@gmail.com>
2024-12-29 20:54:38 -0300

    configuramos los botones para el desplazamiento, ademas en home creamos las secciones para probar este desplazamiento

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -5,8 +5,19 @@ import logoLight from "../../assets/icons/logoLight.png";
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuOpen((prev) => !prev);
+  const toggleMenu = (value) => {
+    setMenuOpen(value);
+  };
+
+  const scrollToSection = (e, id) => {
+    e.preventDefault();
+    const section = document.querySelector(id);
+    const headerHight = 67.92;
+    const sectionPosition = section.offsetTop - headerHight;
+
+    window.scrollTo({ top: sectionPosition, behavior: "smooth" });
+
+    toggleMenu(false);
   };
   return (
     <>
@@ -25,32 +36,33 @@ const Header = () => {
             </a>
             <div className="text-[50px] cursor-pointer">
               {menuOpen ? (
-                <IoClose onClick={() => toggleMenu()} />
+                <IoClose onClick={() => toggleMenu(false)} />
               ) : (
-                <IoMenu onClick={() => toggleMenu()} />
+                <IoMenu onClick={() => toggleMenu(true)} />
               )}
             </div>
           </div>
         </div>
       </div>
+
       {menuOpen && (
-        <div className="absolute top-0 right-0 max-w-[425px] w-full h-screen pt-[67.92px] z-20 bg-slate-50">
-          <a href="#home">
+        <div className="fixed top-0 right-0 max-w-[425px] w-full h-screen pt-[67.92px] z-20 bg-slate-50">
+          <a href="#home" onClick={(e) => scrollToSection(e, "#home")}>
             <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
               Home
             </div>
           </a>
-          <a href="#aboutMe">
+          <a href="#aboutMe" onClick={(e) => scrollToSection(e, "#aboutMe")}>
             <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
               Sobre Mí
             </div>
           </a>
-          <a href="#project">
+          <a href="#project" onClick={(e) => scrollToSection(e, "#project")}>
             <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
               Proyectos
             </div>
           </a>
-          <a href="#contact">
+          <a href="#contact" onClick={(e) => scrollToSection(e, "#contact")}>
             <div className="w-full py-3 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
               Contactos
             </div>
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <div>Home</div>;
+  return <div>
+    <section id="home" className="min-w-full min-h-[500px] bg-red-200">Home</section>
+    <section id="aboutMe" className="min-w-full min-h-[500px] bg-green-200">aboutMe</section>
+    <section id="project" className="min-w-full min-h-[500px] bg-blue-200">project</section>
+    <section id="contact" className="min-w-full min-h-[500px] bg-orange-200">contact</section>
+  </div>;
 };
 
 export default Home;
