84e3982d050ab146b53b77f85708eb745180eed1
Hector <hectorc2907@gmail.com>
2024-12-30 02:19:41 -0300

    configuramos un contexto para el manejo del tema ya sea claro u oscuro y lo implementamos a los cambios para el boton tanto en mobile como en desktop

--- frontend/src/components/common/Header/ToggleThemeDesktop.jsx
+++ frontend/src/components/common/Header/ToggleThemeDesktop.jsx
@@ -1,12 +1,17 @@
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useTheme } from "../../../contexts/ThemeContext";
 
 const ToggleThemeDesktop = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
     <div className="flex items-center gap-2">
-      <button className="font-semibold border-slate-50 border-b-2 hover:border-redIcon">
-        Modo Oscuro
+      <button
+        onClick={() => toggleTheme()}
+        className="font-semibold border-slate-50 border-b-2 hover:border-redIcon"
+      >
+        Modo {theme === "light" ? "Oscuro" : "Claro"}
       </button>
-      <FaMoon />
+      {theme === "light" ? <FaMoon /> : <FaSun />}
     </div>
   );
 };
--- frontend/src/components/common/Header/ToggleThemeMobile.jsx
+++ frontend/src/components/common/Header/ToggleThemeMobile.jsx
@@ -1,10 +1,15 @@
-import { FaMoon } from "react-icons/fa";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useTheme } from "../../../contexts/ThemeContext";
 
 const ToggleThemeMobile = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <button className="w-full py-3 flex justify-center items-center gap-2 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200">
-      <span>Modo Oscuro</span>
-      <FaMoon />
+    <button
+      onClick={() => toggleTheme()}
+      className="w-full py-3 flex justify-center items-center gap-2 text-center text-2xl font-semibold italic hover:bg-slate-200 active:bg-slate-200"
+    >
+      <span>Modo {theme === "light" ? "Oscuro" : "Claro"}</span>
+      {theme === "light" ? <FaMoon /> : <FaSun />}
     </button>
   );
 };
