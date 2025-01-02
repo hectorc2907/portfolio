cc9398eeadcdc42b7516891dddb76733b864e31c
Hector <hectorc2907@gmail.com>
2025-01-02 11:54:36 -0300

    modularizamos la caja de proyectos

--- frontend/src/pages/Home/Projects/Projects.jsx
+++ frontend/src/pages/Home/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import { useTheme } from "../../../contexts/ThemeContext";
-import IconsUtils from "../../../utils/IconsUtils";
+import ProjectsCards from "./ProjectsCards";
 
 const Projects = () => {
   const { theme } = useTheme();
@@ -22,44 +22,7 @@ const Projects = () => {
             </span>
             royectos
           </p>
-          <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            <div className="flex flex-col items-center">
-              <div className="flex flex-col max-w-[250px] rounded-xl shadow-md">
-                <img
-                  src="https://static.vecteezy.com/system/resources/thumbnails/009/273/280/small/concept-of-loneliness-and-disappointment-in-love-sad-man-sitting-element-of-the-picture-is-decorated-by-nasa-free-photo.jpg"
-                  alt="imagen web"
-                  className="rounded-t-xl"
-                />
-                <p className="mt-5 text-center text-2xl font-playFair font-semibold">
-                  Titulo
-                </p>
-                <p className="mt-2 px-3 text-center text-gray-500">
-                  Portafolio moderno y responsivo con React, Tailwind CSS y
-                  animaciones con Motion, destacando proyectos y habilidades.
-                </p>
-                <div className="mt-5 flex justify-evenly text-3xl">
-                  <IconsUtils tech="react.js" />
-                  <IconsUtils tech="tailwindcss" />
-                </div>
-                <div className="mt-2 flex justify-between">
-                  <a
-                    href="#"
-                    target="_blank"
-                    className="w-full flex justify-center items-center gap-2 py-2 text-center border-t-[1px] border-r-[1px] rounded-bl-lg"
-                  >
-                    GitHub <IconsUtils tech="github" />
-                  </a>
-                  <a
-                    href="#"
-                    target="_blank"
-                    className="w-full flex justify-center items-center gap-2 py-2 text-center border-t-[1px] rounded-br-lg"
-                  >
-                    Preview <IconsUtils tech="web" />
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ProjectsCards />
         </div>
       </div>
     </section>
---
+++ frontend/src/pages/Home/Projects/ProjectsCards.jsx
@@ -0,0 +1,64 @@
+import { projects } from "../../../config/projectConfig";
+import { useTheme } from "../../../contexts/ThemeContext";
+import IconsUtils from "../../../utils/IconsUtils";
+
+const ProjectsCards = () => {
+  const { theme } = useTheme();
+  return (
+    <div className="flex flex-col items-center">
+      <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+        {projects.map((project, index) => (
+          <div
+            key={index}
+            className={`max-w-[350px] flex flex-col rounded-xl shadow-md ${
+              theme === "light" ? "" : "shadow-cianIcon"
+            }`}
+          >
+            <img
+              src={project.img}
+              alt={project.title}
+              className="rounded-t-xl"
+            />
+            <p className="mt-5 text-center text-2xl font-playFair font-semibold">
+              {project.title}
+            </p>
+            <p className="mt-2 px-3 text-center text-gray-500">
+              {project.description}
+            </p>
+            <div className="mt-5 flex justify-evenly text-3xl">
+              {project.technologies.map((tech, index) => (
+                <IconsUtils key={index} tech={tech} />
+              ))}
+            </div>
+            <div className="mt-2 flex justify-between">
+              <a
+                href={project.repository}
+                target="_blank"
+                className={`w-full flex justify-center items-center gap-2 py-2 text-center border-t-[1px] border-r-[1px] ${
+                  theme === "light"
+                    ? "hover:bg-gray-150"
+                    : "hover:bg-gray-800 border-cianIcon"
+                } rounded-bl-lg `}
+              >
+                GitHub <IconsUtils tech="github" />
+              </a>
+              <a
+                href={project.preview}
+                target="_blank"
+                className={`w-full flex justify-center items-center gap-2 py-2 text-center border-t-[1px] ${
+                  theme === "light"
+                    ? "hover:bg-gray-150"
+                    : "hover:bg-gray-800 border-cianIcon"
+                } rounded-br-lg `}
+              >
+                Preview <IconsUtils tech="web" />
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsCards;
