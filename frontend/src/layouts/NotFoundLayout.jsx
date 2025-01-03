d96b9757af85eb7311a073b4ac6f523bfa8804f2
Hector <hectorc2907@gmail.com>
2025-01-03 02:15:23 -0300

    configuramos la pagina not found con su layout y sus animaciones

--- frontend/src/layouts/NotFoundLayout.jsx
+++ frontend/src/layouts/NotFoundLayout.jsx
@@ -1,12 +1,18 @@
 import { Outlet } from "react-router-dom";
 import HeaderNotFound from "../components/common/HeaderNotFound/HeaderNotFound";
 import Footer from "../components/common/Footer/Footer";
+import { useTheme } from "../contexts/ThemeContext";
 
 const NotFoundLayout = () => {
+  const { theme } = useTheme();
   return (
     <div className="flex flex-col min-h-screen">
       <HeaderNotFound />
-      <main className="flex-1">
+      <main
+        className={`flex-1 flex justify-center items-center ${
+          theme === "light" ? "" : "bg-gray-950 text-gray-50"
+        }`}
+      >
         <Outlet />
       </main>
       <Footer />
